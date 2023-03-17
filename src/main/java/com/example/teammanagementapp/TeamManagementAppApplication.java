c0086af44bfa25a1aa591858897e88dd2fc79262
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-03-17 21:26:42 +0100

    Initial commit

---
+++ src/main/java/com/example/teammanagementapp/TeamManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.teammanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamManagementAppApplication.class, args);
+	}
+
+}
