d092a752d4e26ab364071de20814c45d23882bf2
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-03-20 19:33:59 +0100

    teamManagement++

---
+++ src/main/java/com/example/persistence/repository/IProjectRepository.java
@@ -0,0 +1,11 @@
+package com.example.persistence.repository;
+
+import com.example.persistence.model.Project;
+
+import java.util.Optional;
+
+public interface IProjectRepository {
+
+    Optional<Project> findById(Long id);
+    Project save (Project project);
+}
