33d10f5aa907feb95f5bcfb2507e6ca295cfa6b7
meetfemisamuel <meetfemisamuel@yahoo.com>
2023-03-23 16:36:21 +0100

    teamManagement++

--- src/main/java/com/example/persistence/repository/impl/ProjectRepositoryImpl.java
+++ src/main/java/com/example/persistence/repository/impl/ProjectRepositoryImpl.java
@@ -2,14 +2,23 @@ package com.example.persistence.repository.impl;
 
 import com.example.persistence.model.Project;
 import com.example.persistence.repository.IProjectRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public class ProjectRepositoryImpl implements IProjectRepository {
 
-    List<Project> projects = new ArrayList<>();
+    @Value("${project.prefix}") // referencing the name of the property
+    private String prefix;
+
+    @Value("${project.suffix}")
+    private Integer suffix;
+
+    private List<Project> projects = new ArrayList<>();
 
     @Override
     public Optional<Project> findById(Long id) {
@@ -19,6 +28,7 @@ public class ProjectRepositoryImpl implements IProjectRepository {
     @Override
     public Project save(Project project) {
         Project existingProject = findById(project.getId()).orElse(null);
+        updateInternalId(project);
         if (existingProject == null) {
             projects.add(project);
         }
@@ -28,5 +38,9 @@ public class ProjectRepositoryImpl implements IProjectRepository {
             projects.add(newProject);
         }
         return project;
+    }
+
+    private void updateInternalId(Project project) {
+        project.setInternalId(prefix + "-" + project.getId() + "-" + suffix);
     }
 }
--- src/main/java/com/example/teammanagementapp/TeamManagementAppApplication.java
+++ src/main/java/com/example/teammanagementapp/TeamManagementAppApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LApplication {
+public class TeamManagementAppApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(LApplication.class, args);
+		SpringApplication.run(TeamManagementAppApplication.class, args);
 	}
 
 }
